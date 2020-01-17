d07014d3b7d028a96fc7f5b0711263c53c6b12f1
lyskoivan <lysko1999i@gmail.com>
2020-01-18 00:06:06 +0200

    Updates

---
+++ precache-manifest.b187524c5df1995670e6be712327937f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac515c6d44503ae164040ad33992b0c7",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "ec858b553503ab5d05d4",
+    "url": "/goit-react-hw-03-phonebook/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "80640527a042ec324c17",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.dc6128b3.chunk.css"
+  },
+  {
+    "revision": "ec858b553503ab5d05d4",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.e78e69e6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.e78e69e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "80640527a042ec324c17",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.bc20f165.chunk.js"
+  },
+  {
+    "revision": "30270bd832f8a1815aaf",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.37614506.js"
+  }
+]);
